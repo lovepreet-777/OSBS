dd627c00ac9a8291451cbcfe4b475e3e982836bd
Lovepreet Singh <96827616+lovepreet-777@users.noreply.github.com>
2022-05-17 20:43:59 +0530

    some changes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -28,6 +28,7 @@ const Login = () => {
         error.response.status <= 500
       ) {
         setError(error.response.data.message);
+        alert(error.response.data.message);
       }
     }
   };
